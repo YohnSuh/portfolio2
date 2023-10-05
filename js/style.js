21da8283c298425b4a5688aa04b641e3f1b797e0
YohnSuh <yohnsuh@gmail.com>
2023-10-05 21:26:31 +0900

    포트폴리오2 제작 중

---
+++ js/style.js
@@ -0,0 +1,15 @@
+// 모바일 햄버거 아이콘 클릭
+function hamMenuClick() {
+    $('.header-bg').toggleClass('mo-bg-on');
+    $('header').toggleClass('mo-menu-on');
+};
+// 모바일 메뉴 배경 클릭
+function mBackgroundOn() {
+    $('.header_bg').removeClass('mo-bg-on');
+    $('header').removeClass('mo-menu-on');
+};
+$(document).ready(function() {
+    // 모바일에서 햄버거 메뉴 클릭 시
+    $('.hamburger').click(hamMenuClick);
+    $('.header-bg').click(mBackgroundOn);
+});
